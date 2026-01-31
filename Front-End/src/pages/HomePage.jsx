c5be2bc31894e256a11b7642fe0b17a688003909
Jacob Latia <jacoblatia@gmail.com>
2026-01-30 20:42:50 -0600

    Removed weather component from home page.

--- Front-End/src/pages/HomePage.jsx
+++ Front-End/src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import './pages.css';
 import HomeNav from '../components/HomeNav';
-import Weather from '../components/Weather';
 
 const HomePage = () => {
     return (
         <>
         <div className="main-content">
             <h1>PennyPulse</h1>
-            <Weather />
             <HomeNav />
         </div>
         </>
